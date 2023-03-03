756a32dde9e3e1e8223e6845861baab863ae2eef
Ooorami <dbstj9867@naver.com>
2023-03-03 13:58:52 +0900

    Implement todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState("");
+  const [list,setList] = useState([]);
+  // [{title: "~~"}, "~~"]
+
+  useEffect ( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response=>response.json())
+        .then(response=>{
+          setList(response.slice(0,9))});
+  },[] )
+
+  const onClick = ()=>{
+    // const copy = [...list];
+    // const newTodo = {title: value}
+    // copy.push(newTodo)
+    // setList(copy)
+    setList(prev=>[...prev, {title: value}]);
+    setValue("");
+  }
+
+  const valueDelete = (id) => {
+    setList((prev) => prev.filter((item) => item.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={value} onChange={(e) => setValue(e.target.value)} placeholder='할 일을 입력하세요.'></input>
+      <button onClick={()=>onClick()}>버튼</button>
+      {value}
+      <ul>
+        {
+          list.map((item)=><li> <button onClick={() => valueDelete(item.id)}>삭제</button> {item.title} </li>)
+        }
+      </ul>
     </div>
   );
 }
-
 export default App;
